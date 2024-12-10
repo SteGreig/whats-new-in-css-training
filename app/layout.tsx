65887646bf169c342cb051bdb61854cb73b5aceb
Stephen Greig <steve.greig1@gmail.com>
2024-12-10 16:21:53 +0000

    Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,22 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const chaneyReg = localFont({
+  src: "./fonts/CHANEY-Regular.otf",
+  variable: "--font-chaney-reg",
+  weight: "400",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+const chaneyWide = localFont({
+  src: "./fonts/CHANEY-Wide.otf",
+  variable: "--font-chaney-wide",
+  weight: "400",
+});
+
+const chaneyUltraExt = localFont({
+  src: "./fonts/CHANEY-UltraExtended.otf",
+  variable: "--font-chaney-reg-ultra-ext",
+  weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -26,8 +33,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${chaneyReg.variable} ${chaneyWide.variable} ${chaneyUltraExt.variable} antialiased`}
       >
+
+        <header className="text-center mt-10">
+          <h1 className="text-5xl text-balance sm:text-[8.5vw] leading-none uppercase tracking-tight ~mb-4/6 sm:px-0">What's new in CSS?</h1>
+          <p className="font-wide ~text-base/3xl tracking-[0.3vw] flex items-center ~gap-4/8 ~px-4/8">
+            <span>19.12.2024</span>
+            <span className="flex-1 h-1 bg-black"></span>
+            <span>Ste Greig</span>
+          </p>
+        </header>
+
         {children}
       </body>
     </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,32 @@
 import type { Config } from "tailwindcss";
+import fluid, { extract } from 'fluid-tailwind'
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: {
+    files: [
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    extract
+  },
   theme: {
+    screens: {
+      "2xs": "23.4375rem", /* 375px */
+      xs: "30rem", /* 480px */
+      sm: "37.5rem", /* 600px */
+      md: "48rem", /* 768px */
+      lg: "64rem", /* 1024px */
+      xl: "80rem", /* 1280px */
+      "2xl": "87.5rem", /* 1400px */
+      "3xl": "100rem", /* 1600px */
+      "4xl": "118.75rem", /* 1900px */
+    },
+    fontFamily: {
+      sans: ["var(--font-chaney-reg)", "sans-serif"],
+      wide: ["var(--font-chaney-wide)", "sans-serif"],
+      ultraext: ["var(--font-chaney-reg-ultra-ext)", "sans-serif"],
+    },
     extend: {
       colors: {
         background: "var(--background)",
@@ -14,5 +34,7 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    fluid
+  ],
 } satisfies Config;
