866e1e75b5cab05ce7abc0a4be0fe56ccd4fb881
Stephen Greig <steve.greig1@gmail.com>
2024-12-18 21:37:17 +0000

    Menu fix

--- app/components/SlideoutMenu.tsx
+++ app/components/SlideoutMenu.tsx
@@ -26,13 +26,13 @@ const SlideoutMenu = () => {
 
       {/* Off-canvas menu */}
       <div
-        className={`${isOpen ? "slideout-open shadow-2xl translate-x-0" : "-translate-x-full"} fixed top-0 left-0 h-screen w-full max-w-screen-sm bg-yellow-500 dark:bg-gray-900 z-50 transition duration-300 ~p-8/14`}
+        className={`${isOpen ? "slideout-open shadow-2xl translate-x-0" : "-translate-x-full"} fixed top-0 left-0 h-screen w-full max-w-screen-sm bg-yellow-500 dark:bg-gray-900 z-50 transition duration-300 ~p-8/14 flex flex-col`}
       >
-        <div className="flex items-center mb-4 gap-4">
+        <div className="flex items-center mb-4 gap-4 flex-none">
           <p className="font-primary font-normal text-xl">Menu</p>
           <span className="h-[2px] flex-1 bg-black dark:bg-yellow-500"></span>
         </div>
-        <nav className="">
+        <nav className="flex-1 overflow-y-auto">
           <ul className="flex flex-col">
             {pages.map((page, i) => (
               <li key={i}>
